11e72e0c119c987b99e7171f9b85108bfed11118
marijaab <k20082541@kcl.ac.uk>
2021-12-02 18:53:44 +0000

    Refactor seed/unseed. Small layout changes

--- chessclubs/management/commands/unseed.py
+++ chessclubs/management/commands/unseed.py
@@ -1,7 +1,10 @@
 """The database unseeder."""
 from django.core.management.base import BaseCommand, CommandError
-from chessclubs.models import User
+from chessclubs.models import User, Club
+from django.contrib.auth.models import Group
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.filter(is_staff=False, is_superuser=False).delete()
+        Club.objects.all().delete()
+        Group.objects.all().delete()
